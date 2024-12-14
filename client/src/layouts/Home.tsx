0f390896270e918dc743bd0496c7352fa65eb513
YassineIDR <yassineidrissi2005@gmail.com>
2024-12-14 03:08:23 +0100

    final version

--- client/src/layouts/Home.tsx
+++ client/src/layouts/Home.tsx
@@ -60,7 +60,7 @@ export const Logo = () => {
         animate={{ opacity: 1 }}
         className="font-medium text-black dark:text-white whitespace-pre"
       >
-        Acet Labs
+        UBO
       </motion.span>
     </Link>
   );
